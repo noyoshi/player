836432eb911df0e9eaf296b455c7eddb30aebae2
Noah Yoshida <nyoshida@nd.edu>
2019-01-27 20:38:26 -0800

    player works

---
+++ README.md
@@ -0,0 +1,12 @@
+Written in **Python 3.5.2** because I can't compile Python 3.7 on my raspberry
+pi...
+
+Make sure you have VLC installed!!! Before pip3 installing python-vlc. DON'T
+INSTALL pip3 install vlc!!! Everything breaks.
+
+Then, you need pulseaudio working...
+
+`sudo apt-get install pulseaudio pulseaudio-module-zeroconf alsa-utils avahi-daemon`
+
+Just do this
+https://raspberrypi.stackexchange.com/questions/639/how-to-get-pulseaudio-running
--- player.py
+++ player.py
@@ -2,48 +2,52 @@ import vlc
 import curses
 from flask import Flask, request
 import os
+
 INSTANCE = vlc.Instance()
 PLAYER = INSTANCE.media_player_new()
-# Media = Instance.media_new('song.mp3')
-# player.set_media(Media)
-# player.play()
-# while True:
-#     c = stdscr.getch()
-#     if c == ord('q'):
-#         player.pause() 
-#     if c == ord('p'):
-#         player.play()
-UPLOAD_FOLDER = '/Users/silver/.config/player/'
+UPLOAD_FOLDER = '{}/.config/player/'.format(os.path.expanduser("~"))
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CURRENT_SONG = None
+SONG_QUEUE = []
+SONG_PLAYING = False
 
 @app.route("/<song_name>")
 def play_file(song_name):
+    CURRENT_SONG = song_name
+    SONG_PLAYING = True
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], song_name)
     media = INSTANCE.media_new(file_path)
     PLAYER.set_media(media)
     PLAYER.play()
-    return f"Playing {song_name}"
+    return "Playing {}".format(song_name)
 
 @app.route("/pause")
 def pause():
     PLAYER.pause()
-    return("Paused!")
+    return "Paused {}".format(CURRENT_SONG)
 
 @app.route("/play")
 def play():
     PLAYER.play()
-    return("Playing!")
+    return "Playing {}".format(CURRENT_SONG)
 
-@app.route("/upload/<file_name>", methods=["POST"])
-def upload(file_name):
-    # print(request.files)
-    # for f in request.files:
-    #     print(f)
-    #     file = request.files[f]
+@app.route("/queue/<song_name>")
+def queue_song(song_name):
+    if SONG_PLAYING:
+        SONG_QUEUE.append(song_name)
+        return "Queued {}".format(song_name)
+    else:
+        return play_file(song_name)
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    # Get rid of extension
     file = request.files['file']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    return f"Uploaded ?"
+    file_name = file.filename.split(".")[0]
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+    return "Uploaded {}".format(file.filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
